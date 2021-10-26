28645669648ac3c5b13bc6b09c82ba3dd0d48c1f
Artem Vovchenko <a.vovchenko01@gmail.com>
2021-10-26 22:16:00 +0300

    Update for lab2

--- lab2/services/service_js/service-js-2/service-js-2.js
+++ lab2/services/service_js/service-js-2/service-js-2.js
@@ -1,9 +1,25 @@
-const http = require('http');
+const express = require('express')
+const app = express()
 
-const requestListener = function (req, res) {
-  res.writeHead(200);
-  res.end('Hello World from Node server!');
-}
+var broken = false
 
-const server = http.createServer(requestListener);
-server.listen(8080);
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+  if (broken) {
+    setTimeout( () => {
+        res.send('hello world! You have experienced a 5 sec lag ...')
+    }, 5000 ) 
+  } else {
+    res.send("Hello world!")
+  }
+})
+
+app.get('/break', function (req, res) {
+  broken = true 
+  res.send("You have successfully broken your service!")
+})
+
+app.listen(8080, () => {
+  console.log(`Server is working on port 8080`)
+})
+
