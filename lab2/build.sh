0759a5fcc192aef701deef9a64427ca313281950
Artem Vovchenko <a.vovchenko01@gmail.com>
2021-10-27 09:25:30 +0300

    Add scripts for building images and starting cluster

---
+++ lab2/build.sh
@@ -0,0 +1,9 @@
+if ! minikube ip; then
+	minikube start
+fi
+eval $(minikube -p minikube docker-env)
+curr=$(pwd)
+cd $curr/services/service_py              && docker build -t avovchenko01/service-py .
+cd $curr/services/service_js/service-js-1 && docker build -t avovchenko01/service-js-1 .
+cd $curr/services/service_js/service-js-2 && docker build -t avovchenko01/service-js-2 .
+
